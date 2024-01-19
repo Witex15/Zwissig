cf94841b2a453901dd17d0efbcb97538a97abdea
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-01-19 14:52:59 +0100

    add default value to read column

--- database/migrations/2024_01_19_130730_add_reads_to_articles_table.php
+++ database/migrations/2024_01_19_130730_add_reads_to_articles_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->integer("reads");
+            $table->integer("reads")->default(0);
         });
     }
 
